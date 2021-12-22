691ef9ea7bce09d1ef6f0078e7d8ae41aa7bae9a
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2021-12-22 14:15:07 +0200

    fix bug

--- admin.js
+++ admin.js
@@ -95,7 +95,68 @@ const app = Vue.createApp({
       const data = snapshot.val()
       this.users = data
       const  arr_uses = Object.keys(data).map((key) => data[key])
-      console.log(arr_uses);
+      this.votes = [
+        {
+          id: 1,
+          name: 'Николаевский филиал',
+          votes: 0
+        },
+        {
+          name: 'Полтавский филиал',
+          id: 2,
+          votes: 0
+        },
+        {
+          name: ' Фронт-офис',
+          id: 3,
+          votes: 0
+        },
+        {
+          name: ' Днепровский филиал',
+          id: 4,
+          votes: 0
+        },
+        {
+          name: 'Запорожский филиал',
+          id: 5,
+          votes: 0
+        },
+        {
+          name: 'Ивано-Франковский филиал',
+          id: 6,
+          votes: 0
+        },
+        {
+          name: 'Харьковский филиал',
+          id: 7,
+          votes: 0
+        },
+        {
+          name: 'ТОМО',
+          id: 8,
+          votes: 0
+        },
+        {
+          name: 'Киевский филиал',
+          id: 9,
+          votes: 0
+        },
+        {
+          name: 'Одесский филиал',
+          id: 10,
+          votes: 0
+        },
+        {
+          name: 'Инженерный отдел',
+          id: 11,
+          votes: 0
+        },
+        {
+          name: 'Львовский филиал',
+          id: 12,
+          votes: 0
+        }
+      ]
       this.votes.forEach(v => {
         arr_uses.forEach(user => {
           if (user.votes.includes(v.id)) {
